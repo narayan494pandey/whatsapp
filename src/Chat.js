bd472ba911dd7bdafbbad8e2fa85366916e0574e
Narayan Pandey <npandey722@gmail.com>
2020-11-19 19:24:21 +0530

    chat_header

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import './Chat.css'
-import ChatHeader from './ChatHeader'
+import { Avatar, IconButton } from '@material-ui/core'
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
+
 
 function Chat() {
     return (
         <div className="chat">
-            <ChatHeader />
+            <div className="chat__Header">
+                <Avatar />
+                <div className="Chat__HeaderInfo">
+                    <h3>Room Name</h3>
+                    <p>Last seen at...</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                   
+                </div>
+            </div>
         </div>
     )
 }
