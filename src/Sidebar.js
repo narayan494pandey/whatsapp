efb5f488ff45f2f980e24eb01de1035186168b18
Narayan Pandey <npandey722@gmail.com>
2020-11-19 17:32:41 +0530

    sidebar header

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import './Sidebar.css'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import  {Avatar,IconButton} from  '@material-ui/core'
 function Sidebar() {
     return (
         <div className="sidebar">
-            Sidebar
+            <div className="sidebar__header">
+            <Avatar src="/images/Narayan.jpg" />
+            <div className="sidebar__headerRight">
+                <IconButton>
+                 <DonutLargeIcon />
+                </IconButton>
+                <IconButton>
+                 <ChatIcon />
+                </IconButton>
+                <IconButton>
+                 <MoreVertIcon />
+                </IconButton>
+                
+            </div>
+
+            
+            
+            </div>
         </div>
     )
 }
