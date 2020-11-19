84b7995e0e3477aa70dd59d3dd0dfaeb5c40be2b
Narayan Pandey <npandey722@gmail.com>
2020-11-19 15:08:26 +0530

    styles

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import Sidebar from "./Sidebar"
 function App() {
   return (
     <div className="app">
+      <div className="app__body">
       <Sidebar />
       <ChatBox />
+      
+      </div>
+        
     </div>
   );
 }
